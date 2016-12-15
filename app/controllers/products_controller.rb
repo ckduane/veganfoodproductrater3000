4e61f7a3de9f55d1fab77834c94e9f378336ab12
ckduane <ckduane@gmail.com>
2016-12-14 22:28:13 -0800

    add new product feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,31 @@
 class ProductsController < ApplicationController
+
+	def new
+		@product = Product.new
+		@categories = Category.all
+	end
+
 	def show
 		@product = Product.find_by(id: params[:id])
+	end
+
+	def create
+		@product = Product.new
+		@brand = Brand.find_or_create_by(name: params[:product][:brand])
+		@product.brand_id = @brand.id
+		@product.name = params[:product][:name]
+		@product.category_id = params[:product][:category_id]
+		if @product.save
+			redirect_to product_path(@product)
+		else
+			@errors = @product.errors.full_messages
+			render :new
+		end
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:category_id, :brand, :name)
 	end
 end
